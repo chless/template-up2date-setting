833e2da522c06c8a88bd3b93440b6d4fa43adde7
chanhui lee <chlessling@gmail.com>
2022-08-08 17:01:46 +0900

    refactor: var name change

--- arguments.py
+++ arguments.py
@@ -3,10 +3,10 @@ import argparse
 
 def argument_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--cfg_data", type=str)
-    parser.add_argument("--cfg_model", type=str)
-    parser.add_argument("--cfg_optimizer", type=str)
-    parser.add_argument("--cfg_trainer", type=str)
+    parser.add_argument("--cfg_data_path", type=str)
+    parser.add_argument("--cfg_model_path", type=str)
+    parser.add_argument("--cfg_optimizer_path", type=str)
+    parser.add_argument("--cfg_trainer_path", type=str)
 
     args = parser.parse_args()
     return args
