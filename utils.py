b6c9b418a60639b76a9adf712d01b0c7d66b1847
chanhui lee <chlessling@gmail.com>
2022-08-14 14:22:33 +0900

    feat: main cfg add
    gpu specification and each cfg path is maintained in cfg_main.
    user input onlyt cfg_main_path

--- utils.py
+++ utils.py
@@ -7,17 +7,20 @@ def init_neptune(cfg):
     run["cfg"] = cfg
     return run
 
+
 def get_total_cfg(args):
-    cfg_data = OmegaConf.load(args.cfg_path_data)
-    cfg_optimizer = OmegaConf.load(args.cfg_path_optimizer)
-    cfg_model = OmegaConf.load(args.cfg_path_model)
-    cfg_trainer = OmegaConf.load(args.cfg_path_trainer)
+    cfg_main = OmegaConf.load(args.cfg_path_main)
+    cfg_data = OmegaConf.load(cfg_main.cfg_path_data)
+    cfg_optimizer = OmegaConf.load(cfg_main.cfg_path_optimizer)
+    cfg_model = OmegaConf.load(cfg_main.cfg_path_model)
+    cfg_trainer = OmegaConf.load(cfg_main.cfg_path_trainer)
     cfg = OmegaConf.create(
         {
+            "main": cfg_main,
             "data": cfg_data,
             "model": cfg_model,
             "trainer": cfg_trainer,
-            "optimizer": cfg_optimizer
-            }
+            "optimizer": cfg_optimizer,
+        }
     )
     return cfg
