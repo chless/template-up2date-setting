609bc74f413feb802559fd0897c3adad60e422a4
chless <chlessling@gmail.com>
2022-12-07 13:37:33 +0900

    feat: structured cfg

--- utils.py
+++ utils.py
@@ -2,5 +2,7 @@ from omegaconf import OmegaConf
 
 
 def get_cfg(args):
-    cfg = OmegaConf.load(args.cfg_path_main)
-    return cfg
+    cfg = OmegaConf.load(args.cfg_path)
+
+    structured_cfg = OmegaConf.structured(cfg)
+    return structured_cfg
