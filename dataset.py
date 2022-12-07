217014f84392eb95a58754164271acc8d067dc85
chless <chlessling@gmail.com>
2022-12-07 13:51:10 +0900

    refactor: deprecate modules

--- dataset.py
+++ dataset.py
@@ -14,23 +14,3 @@ class Dataset(torch.utils.data.Dataset):
     def __getitem__(self, idx):
         raise NotImplementedError
 
-
-class HDF5_Dataset(torch.utils.data.Dataset):
-    def __init__(self, data_path):
-        self.data_path = data_path
-
-    def __len__(self):
-        if not hasattr(self, "len"):
-            data = h5py.File(self.data_path, "r")
-            self.len = data["/x"].shape[0]
-            data.close()
-            return self.len
-        else:
-            return self.len
-
-    def __getitem__(self, idx):
-        data = h5py.File(osp.join(self.data_path, self.data_name), "r")
-        x = data["/x"][idx]
-        y = data["/y"][idx]
-        data.close()
-        return x, y
